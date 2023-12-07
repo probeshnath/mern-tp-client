3c099061e62b0ee2fc6dc7c341cb9a1f051c869b
probeshnath <probeshn@gmail.com>
2023-12-07 11:58:33 +0600

    react router added to go another page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Services from './pages/Services'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
