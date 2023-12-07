5e994d31d0e49af7b1d7a85fbf448046a1eb0774
probeshnath <probeshn@gmail.com>
2023-12-07 22:38:13 +0600

    home and about page added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import { useState } from "react";
 
-const Contact = () => {
+ const Contact = () => {
+  const [contact, setContact] = useState({
+    username: "",
+    email: "",
+    message: "",
+  });
+
+  // lets tackle our handleInput
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setContact({
+      ...contact,
+      [name]: value,
+    });
+  };
+
+  // handle fomr getFormSubmissionInfo
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(contact);
+  };
+
+
+
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <section className="section-contact">
+        <div className="contact-content container">
+          <h1 className="main-heading">contact us</h1>
+        </div>
+        {/* contact page main  */}
+        <div className="container grid grid-two-cols">
+          <div className="contact-img">
+            <img src="https://www.shutterstock.com/image-vector/contact-us-little-people-write-260nw-1744040345.jpg" alt="we are always ready to help" />
+          </div>
 
-export default Contact
+          {/* contact form content actual  */}
+          <section className="section-form">
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="username">username</label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  autoComplete="off"
+                  value={contact.username}
+                  onChange={handleInput}
+                  required
+                />
+              </div>
+
+              <div>
+                <label htmlFor="email">email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  autoComplete="off"
+                  value={contact.email}
+                  onChange={handleInput}
+                  required
+                />
+              </div>
+
+              <div>
+                <label htmlFor="message">message</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  autoComplete="off"
+                  value={contact.message}
+                  onChange={handleInput}
+                  required
+                  cols="30"
+                  rows="6"
+                ></textarea>
+              </div>
+
+              <div>
+                <button type="submit">submit</button>
+              </div>
+            </form>
+          </section>
+        </div>
+
+        <section className="mb-3">
+          {/* <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.2613173278896!2d73.91411937501422!3d18.562253982539413!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c147b8b3a3bf%3A0x6f7fdcc8e4d6c77e!2sPhoenix%20Marketcity%20Pune!5e0!3m2!1sen!2sin!4v1697604225432!5m2!1sen!2sin"
+            width="100%"
+            height="450"
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe> */}
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3618.435552561305!2d91.82933827414313!3d24.917228042985993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3750556002144eab%3A0xe277e14dbca9f2ab!2sShahjalal%20University%20of%20Science%20and%20Technology!5e0!3m2!1sen!2sbd!4v1701966476031!5m2!1sen!2sbd" width="100%" height="450"  allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        </section>
+      </section>
+    </>
+  );
+};
+export default Contact;
