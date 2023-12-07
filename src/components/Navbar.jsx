ff9d7bed2e00b5be933b7618b2d14b31c02fced5
probeshnath <probeshn@gmail.com>
2023-12-07 20:48:11 +0600

    registration form create and data store in vaiable

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 
                             <li><NavLink to="/">Home</NavLink></li>
                             <li><NavLink to="/about">About</NavLink></li>
-                            <li><NavLink to="/conatct">Contact</NavLink></li>
+                            <li><NavLink to="/contact">Contact</NavLink></li>
                             <li><NavLink to="/services">Services</NavLink></li>
                             <li><NavLink to="login">Login</NavLink></li>
                             <li><NavLink to="/register">Register</NavLink></li>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
+    const [user, setUser] = useState({
+        username: "",
+        email: "",
+        phone: "",
+        password: ""
+    })
+
+    // nadle input
+    const handleInput = (e) =>{
+        // console.log(e)
+        let name = e.target.name;
+        let value = e.target.value;
+
+        setUser({
+            ...user,
+            [name]: value,
+        })
+    }
+    // submit
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        // alert(user)
+        console.log(user)
+    }
   return (
-    <div>Register</div>
+    <>
+    <section>
+        <main>
+            <div className="section-registration">
+                <div className="container grid grid-two-cols">
+                    <div className="registration-image">
+                        <img src="https://www.allen.ac.in/ace2324/assets/images/register.png" alt="registration image" width="500" height="500" />
+                    </div>
+                    {/* form */}
+                    <div className="registration-form">
+                        <h1 className='main-heading mb-3'>Registration Form</h1>
+                        <br />
+
+                        <form onSubmit={handleSubmit}>
+                            <div>
+                                <label htmlFor="username">Username</label>
+                                <input type="text" name='username' value={user.username} onChange={handleInput} required autoComplete='off' placeholder='Enter your username...' />
+                            </div>
+                            <div>
+                                <label htmlFor="email">Email</label>
+                                <input type="email" name='email' value={user.email} onChange={handleInput} required autoComplete='off' placeholder='Enter your email...' />
+                            </div>
+                            <div>
+                                <label htmlFor="phone">Phone</label>
+                                <input type="number" name='phone' required value={user.phone} onChange={handleInput} autoComplete='off' placeholder='Enter your phone...' />
+                            </div>
+                            <div>
+                                <label htmlFor="password">password</label>
+                                <input type="password" name='password' value={user.password} onChange={handleInput} required autoComplete='off' placeholder='Enter your password...' />
+                            </div>
+                            <br />
+                            <button className='btn btn-submit' type='submit'>Register Now</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </section>
+    </>
   )
 }
 
